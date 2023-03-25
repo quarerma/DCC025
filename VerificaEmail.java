010772574becffc579aa22cd5586719c5f027e3e
quarerma <gabriel.oliveira.quaresma@gmail.com>
2023-03-25 16:48:00 -0300

    Adicionando Verificador de Email

---
+++ VerificaEmail.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.verificaemail;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class VerificaEmail {
+
+   public static boolean verficador(String email)
+   {
+       int at = 0;       
+       //Busca o caractere @ a partir da primeira letra
+       at = email.indexOf("@", 1);
+       //Checa se o email possui @
+       if(at == -1) return false;
+       //Checar se possui .com a partir do @
+       if(email.indexOf(".com", at + 1) == -1) return false;
+       
+       return true;
+       
+       
+               
+               
+   }
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Digite um email:");
+        String email = teclado.nextLine().trim();
+        
+        if(!verficador(email)) System.out.println("Enail inválido");
+        else System.out.println("Email valido");
+    }
+}
